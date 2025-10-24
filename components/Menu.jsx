cc2d367e39a2f4b61e9bc06dcb98f3e274e8b2aa
bjamyl <alhassanjamil0@gmail.com>
2025-10-24 04:10:43 +0000

    fix: fix bugs and typo in menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -89,64 +89,66 @@ export default function Menu({ toggle, setToggle }) {
     {
       id: 3,
       linkName: "CONTACT",
-      num: "04",
+      num: "03",
       address: "/contact",
       ref: contact,
     },
   ];
 
+  const isActive = (path) => {
+    if (path === "/") return router.pathname === "/";
+    return router.pathname.startsWith(path);
+  };
+
   return (
-      <section ref={container} className="h-screen w-screen fixed top-0 z-50 ">
-        <div ref={bg} className="invisible relative w-full h-full">
-          <div
-            ref={topHalf}
-            id="upper"
-            className="invisible top-0  h-1/2 bg-[#1C1D1F] z-40"
-          ></div>
-          <div
-            ref={bottomHalf}
-            id="lower"
-            className="invisible bottom-0 h-1/2 bg-[#1C1D1F]"
-          ></div>
-          <div className="absolute top-0 w-full h-full flex flex-col justify-center px-10 xl:px-20 2xl:mx-64">
-            <div className="h-2/4 flex flex-col justify-between">
-              <ul className="text-slate-50  font-gilroyBold text-4xl space-y-6">
-                {menuData.map((menuItem) => (
-                  <li
-                    onClick={() => setToggle(false)}
-                    className={`hover:cursor-pointer font-gilroyBold invisible overflow-hidden xl:text-7xl ${
-                      router.pathname === menuItem.address
-                        ? "text-[#ed9022]"
-                        : ""
-                    }`}
-                    key={menuItem.id}
-                    ref={menuItem.ref}
-                  >
-                    <Link href={menuItem.address}>
-                      <span className="text-sm xl:text-3xl">
-                        {menuItem.num}
-                      </span>
-                      {menuItem.linkName}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              <ul className="flex gap-4">
-                {links.map((link) => (
-                  <li
-                    key={link.id}
-                    className="hover:cursor-pointer hover:text-[#ed9022] font-bold font-gilroyLight invisible  text-slate-50 underline"
-                    ref={link.ref}
-                  >
-                    <a href={link.address} target="_blank" rel="noreferrer">
-                      {link.linkName}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+    <section ref={container} className="h-screen w-screen fixed top-0 z-50 ">
+      <div ref={bg} className="invisible relative w-full h-full">
+        <div
+          ref={topHalf}
+          id="upper"
+          className="invisible top-0  h-1/2 bg-[#1C1D1F] z-40"
+        ></div>
+        <div
+          ref={bottomHalf}
+          id="lower"
+          className="invisible bottom-0 h-1/2 bg-[#1C1D1F]"
+        ></div>
+        <div className="absolute top-0 w-full h-full flex flex-col justify-center px-10 xl:px-20 2xl:mx-64">
+          <div className="h-2/4 flex flex-col justify-between">
+            <ul className="text-slate-50 font-gilroyBold text-4xl space-y-6">
+              {menuData.map((menuItem) => (
+                <li
+                  onClick={() => setToggle(false)}
+                  className={`hover:cursor-pointer font-gilroyBold invisible overflow-hidden xl:text-7xl ${
+                    isActive(menuItem.address) ? "text-[#ed9022]" : ""
+                  }`}
+                  key={menuItem.id}
+                  ref={menuItem.ref}
+                >
+                  <Link href={menuItem.address}>
+                    <span className="text-sm xl:text-3xl">{menuItem.num}</span>
+                    {menuItem.linkName}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+
+            <ul className="flex gap-4">
+              {links.map((link) => (
+                <li
+                  key={link.id}
+                  className="hover:cursor-pointer hover:text-[#ed9022] font-bold font-gilroyLight invisible  text-slate-50 underline"
+                  ref={link.ref}
+                >
+                  <a href={link.address} target="_blank" rel="noreferrer">
+                    {link.linkName}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   );
 }
